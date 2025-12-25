c90a7d3c142b0851f9a544ba807e4260ca3b8beb
unix2dossss <vandan.ubuntu@gmail.com>
2025-12-25 06:51:40 +0000

    Create telegram notification bot

---
+++ notification.py
@@ -0,0 +1,19 @@
+import requests
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if not BOT_TOKEN or not CHAT_ID:
+    raise RuntimeError("Missing Telegram env vars")
+
+def notify(text: str):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    r = requests.post(url, data={"chat_id": CHAT_ID, "text": text}, timeout=10)
+    print(r.status_code, r.text)
+
+notify("Motion detected!")
